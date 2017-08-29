f293f4a56055203355744e6f534aed7501c6bb78
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-08-29 14:01:02 -0600

    time differences added and formatted

--- assets/js/trainSchedule.js
+++ assets/js/trainSchedule.js
@@ -18,9 +18,14 @@ $(document).ready(function () {
       event.preventDefault();
 
       var trainName = $("#trainName").val().trim();
-      var trainDestination =$("#trainDestination").val().trim(); 
+      var trainDestination = $("#trainDestination").val().trim(); 
       var trainFirstTrain = $("#trainFirstTrain").val().trim();
-      var trainFrequency = $("#trainFrequency").val().trim(); 
+      var trainFrequency = $("#trainFrequency").val().trim();
+      
+      $("#trainName").val("")
+      $("#trainDestination").val("")
+      $("#trainFirstTrain").val("")
+      $("#trainFrequency").val("")
       
       var trainObject = {
         name: trainName,
@@ -38,54 +43,52 @@ $(document).ready(function () {
 
     database.ref().on("child_added", function(snapshot, prevChildKey) {
 
-      console.log("check for: " + snapshot.val());
-      console.log(snapshot.val());
+      // console.log("check for: " + snapshot.val());
+      // console.log(snapshot.val());
       var trainObject = snapshot.val().trainObject;
-        console.log(trainObject);
+      // console.log(trainObject);
 
 
       var trainName = trainObject.name;
       var trainDestination = trainObject.destination;
       var trainFirstTrain = trainObject.firstTrain;
       var trainFrequency = trainObject.frequency;
-      var trainNextArrival = 123;
-      var trainMinutesAway = 456;
 
-      var totalMonths = 1 //calculateMonthsOfPay(empStartLatest);
+      var now = new Date();
+      var nowHourMinute = moment(now).format("HH:mm");      
+
+      var trainNextArrival = checkForNextTrainTime(trainFirstTrain, trainFrequency);
+      var trainMinutesAway = moment(moment(trainNextArrival, "HH:mm").diff(moment(), "minutes"));
 
        $(".table > tbody").append(
-        "<tr><td>" + trainName + 
+          "<tr><td>"  + trainName + 
           "</td><td>" + trainDestination + 
           "</td><td>" + trainFirstTrain + 
           "</td><td>" + trainFrequency + 
           "</td><td>" + trainNextArrival + 
           "</td><td>" + trainMinutesAway
         + "</td></tr>");
-    })
+    });
 
+  function checkForNextTrainTime(trainFirstTrain, trainFrequency) {
+    var now = new Date();
+    var nowUnix = moment(now).format("x");
+    var nowHourMinuteFormatted = moment(now).format("HH:mm");
+    var returnTrainTime = moment(trainFirstTrain, "HH:mm");
 
-// function calculateMonthsOfPay (startDate) {
-// currentDate = new Date()
-// // full months
-// var months = monthDiff(startDate, currentDate);
-// var startDateAsDate = new Date(startDate);
+    // if we're not past first train of the day, we use first train of the day
+    if (nowUnix < returnTrainTime) {
+      // console.log("First train hasn't gone yet - it is at: " + returnTrainTime)
+      returnTrainTime = moment(returnTrainTime).format("HH:mm");
+    }
+    else {
+      // add the frequency to the first train time (stored as return train time) until it's greater then the current time - this is our next train
+      do { 
+      returnTrainTime = moment(returnTrainTime, "HH:mm").add(trainFrequency, 'minutes').format("HH:mm")
+      } while (nowHourMinuteFormatted > returnTrainTime);
+    }
 
-// // part of month
-// var numberOfDays = startDateAsDate.getDate();
-// var numberOfPaidDays = (30 - numberOfDays);
-// var numberOfPaidPartialMonths = (numberOfPaidDays / 30);
-// months += numberOfPaidPartialMonths;
-// return months
-// }
+    return returnTrainTime
+  }
 
-
-// function monthDiff(d1String, d2String) { // props to:  https://stackoverflow.com/questions/2536379/difference-in-months-between-two-dates-in-javascript
-//     var months;
-//     var d1 = new Date(d1String);
-//     var d2 = new Date(d2String);
-//     months = (d2.getFullYear() - d1.getFullYear()) * 12;
-//     months -= d1.getMonth() + 1;
-//     months += d2.getMonth();
-//     return months <= 0 ? 0 : months;
-// }
-})
+});
